6478319a8d95f60d7d477c0f1e8ab2b8327dc8d8
房明亮 <12571771+bright-room@user.noreply.gitee.com~>
2023-05-07 22:19:16 +0800

    5.7第一次提交

--- src/test/java/com/itheima/reggie/Solution.java
+++ src/test/java/com/itheima/reggie/Solution.java
@@ -1,32 +1,31 @@
 package com.itheima.reggie;
 
-import com.alibaba.druid.sql.visitor.functions.Char;
-import org.apache.ibatis.logging.stdout.StdOutImpl;
 import org.junit.jupiter.api.Test;
-import org.mockito.invocation.MatchableInvocation;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import java.rmi.MarshalException;
 import java.util.*;
-import java.util.stream.Collectors;
 
 
 class Solution {
-
     @Test
     public void test() {
-        System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
-        System.out.println("心之刚");
-        System.out.println("master");
-        System.out.println("hot-fix");
-        System.out.println("hhh");
-        System.out.println("push test");
-        System.out.println("pull代码");
+
     }
-
-
-
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        HashMap<Integer,Integer> map1 = new HashMap<>();
+        int res = 0;
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                map1.put(nums1[i] + nums2[j],map1.getOrDefault(nums1[i] + nums2[j],0) + 1);
+            }
+        }
+        for (int i = 0; i < nums3.length; i++) {
+            for (int j = 0; j < nums4.length; j++) {
+                int sum = nums3[i] + nums4[j];
+                Integer val = map1.getOrDefault(-sum, 0);
+                res += val;
+            }
+        }
+        return res;
+    }
 
 }
